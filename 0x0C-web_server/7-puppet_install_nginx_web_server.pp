d6c19b30e2c1ceebbcc76d89fc544a3e7abe7aef
behailu <behailuis4simachew@gmail.com>
2022-12-14 10:10:57 -0800

    Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# install nginx
+package { 'nginx':
+  ensure => installed,
+  }
+
+file_line { 'aaaaa':
+ ensure => 'present',
+ path   => '/etc/nginx/sites-available/default',
+ after  => 'listen 80 default_server;',
+ line   => 'rewrite ^/redirect_me https://www.ethiopianorthodox.org/english/history.html permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
